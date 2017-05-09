320c281001700d258b8a52cff8bca4178a83a842
Gregory May <mrgregmay@gmail.com>
2017-05-09 20:18:25 +0200

    Increase xVMHyperV VM guest StartupMemory max limit and MinimumMemory max limit from 17GB to 64GB
    Fixes [#93](https://github.com/PowerShell/xHyper-V/issues/93)

--- DSCResources/MSFT_xVMHyperV/MSFT_xVMHyperV.psm1
+++ DSCResources/MSFT_xVMHyperV/MSFT_xVMHyperV.psm1
@@ -124,11 +124,11 @@ function Set-TargetResource
         [UInt32]$Generation = 1,
 
         # Startup RAM for the VM
-        [ValidateRange(32MB,17342MB)]
+        [ValidateRange(32MB,65536MB)]
         [UInt64]$StartupMemory,
 
         # Minimum RAM for the VM. This enables dynamic memory
-        [ValidateRange(32MB,17342MB)]
+        [ValidateRange(32MB,65536MB)]
         [UInt64]$MinimumMemory,
 
         # Maximum RAM for the VM. This enables dynamic memory
