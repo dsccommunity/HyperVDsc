1012d1eb5c3c5e3465bdb9a496b0022a259008d5
Chirishman <Chirishman@users.noreply.github.com>
2017-12-05 10:01:37 -0500

    Ignore Recovery Partitions
    Fixes presence of recovery partitions breaking functionality. Adding specification of a numbered partition can be enhanced under a separate cover if desired, but I can't see a scenario where anyone is intentionally injecting contents into a VM's recovery partition so it seems safe to filter out that partition type.

--- DSCResources/MSFT_xVhdFileDirectory/MSFT_xVhdFileDirectory.psm1
+++ DSCResources/MSFT_xVhdFileDirectory/MSFT_xVhdFileDirectory.psm1
@@ -38,7 +38,7 @@ function Get-TargetResource
     $itemsFound = foreach($Item in $FileDirectory)
     {
         $item = GetItemToCopy -item $item
-        $mountedDrive =  $mountVHD | Get-Disk | Get-Partition | Get-Volume
+        $mountedDrive =  $mountVHD | Get-Disk | Get-Partition | ?{$_.Type -ne 'Recovery'} | Get-Volume
         $letterDrive  = (-join $mountedDrive.DriveLetter) + ":\"
        
         # show the drive letters.
@@ -98,7 +98,7 @@ function Set-TargetResource
             # show the drive letters.
             Get-PSDrive | Write-Verbose
 
-            $mountedDrive = $mountedVHD | Get-Disk | Get-Partition | Get-Volume
+            $mountedDrive = $mountedVHD | Get-Disk | Get-Partition | ?{$_.Type -ne 'Recovery'} | Get-Volume
             
             foreach ($item in $FileDirectory)
             {
@@ -185,7 +185,7 @@ function Test-TargetResource
         # Show the drive letters after mount 
         Get-PSDrive | Write-Verbose
 
-        $mountedDrive = $mountedVHD | Get-Disk | Get-Partition | Get-Volume
+        $mountedDrive = $mountedVHD | Get-Disk | Get-Partition | ?{$_.Type -ne 'Recovery'} | Get-Volume
         $letterDrive  = (-join $mountedDrive.DriveLetter) + ":\"
         Write-Verbose $letterDrive
 
