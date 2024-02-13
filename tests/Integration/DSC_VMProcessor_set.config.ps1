01e4aa41b9622c6ba5ef9b90bab6340ae5040e5a
gplenty <156962094+gplenty@users.noreply.github.com>
2024-02-13 20:54:13 +0100

    Update DSC_VMProcessor_set.config.ps1
    No idea why it still has xVMProcessor Integration_Test defined in the  tests/Integration/DSC_VMProcessor_set.config.ps1

--- tests/Integration/DSC_VMProcessor_set.config.ps1
+++ tests/Integration/DSC_VMProcessor_set.config.ps1
@@ -3,7 +3,7 @@ configuration DSC_VMProcessor_Set_Config {
     Import-DscResource -ModuleName 'HyperVDsc'
 
     node localhost {
-        xVMProcessor Integration_Test {
+        DSC_VMProcessor Integration_Test {
             VMName                                       = $Node.VMName
             CompatibilityForMigrationEnabled             = $Node.CompatibilityForMigrationEnabled
             CompatibilityForOlderOperatingSystemsEnabled = $Node.CompatibilityForOlderOperatingSystemsEnabled
