c070c88229b3ebf5f2c30e8a0d9e67b447c7ab67
gplenty <156962094+gplenty@users.noreply.github.com>
2024-02-14 12:52:54 +0100

    Update DSC_VMProcessor_set.config.ps1

--- tests/Integration/DSC_VMProcessor_set.config.ps1
+++ tests/Integration/DSC_VMProcessor_set.config.ps1
@@ -3,7 +3,7 @@ configuration DSC_VMProcessor_Set_Config {
     Import-DscResource -ModuleName 'HyperVDsc'
 
     node localhost {
-        DSC_VMProcessor Integration_Test {
+        xVMProcessor Integration_Test {
             VMName                                       = $Node.VMName
             CompatibilityForMigrationEnabled             = $Node.CompatibilityForMigrationEnabled
             CompatibilityForOlderOperatingSystemsEnabled = $Node.CompatibilityForOlderOperatingSystemsEnabled
